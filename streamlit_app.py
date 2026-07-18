5bf6e2e5ad1835e5bb43ddc99daa1e27613933f3
VIJAY009876 <your-email@example.com>
2026-07-18 15:36:44 +0530

    Integrate ChatMemory with Streamlit session state

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from src.llm.client import ask_llm
+from src.memory.chat_memory import ChatMemory
 
 st.set_page_config(
     page_title="AI Research Assistant",
@@ -10,14 +11,27 @@ st.set_page_config(
 st.title("🤖 AI Research & Knowledge Assistant")
 st.write("Welcome! Ask me anything.")
 
+if "memory" not in st.session_state:
+    st.session_state.memory = ChatMemory()
+
+for message in st.session_state.memory.get_conversation():
+
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
 user_prompt =  st.chat_input("Ask anything...")
 
+
+
 if user_prompt:
+    st.session_state.memory.add_user_message(user_prompt)
     with st.chat_message("user"):
         st.markdown(user_prompt)
 
 if user_prompt:
     response = ask_llm(user_prompt)
+    st.session_state.memory.add_assistant_message(response)
     with st.chat_message("assistant"):
         st.markdown(response)
     
